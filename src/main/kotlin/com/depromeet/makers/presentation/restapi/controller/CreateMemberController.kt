6d8039c2c3c913ac042c0260118b744f09457af2
송영민 <jakgon@naver.com>
2024-04-28 13:41:13 +0900

    refactor: 단일 목적 컨트롤러로 변경

--- src/main/kotlin/com/depromeet/makers/presentation/restapi/controller/CreateMemberController.kt
+++ src/main/kotlin/com/depromeet/makers/presentation/restapi/controller/CreateMemberController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/v1/members")
-class MemberController(
+class CreateMemberController(
     private val createNewMember: CreateNewMember,
 ) {
     @PostMapping
