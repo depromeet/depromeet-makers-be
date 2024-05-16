8c183331eb11c169fd554d0311eb7ac0fb2fb772
송영민 (YeongMin Song) <jakgon@naver.com>
2024-05-16 13:27:10 +0900

    feat: 이메일 존재유무 검증 API 추가 (#19)

--- src/main/kotlin/com/depromeet/makers/presentation/restapi/controller/AuthController.kt
+++ src/main/kotlin/com/depromeet/makers/presentation/restapi/controller/AuthController.kt
@@ -1,18 +1,23 @@
 package com.depromeet.makers.presentation.restapi.controller
 
+import com.depromeet.makers.domain.exception.MemberNotFoundException
 import com.depromeet.makers.domain.usecase.GenerateTokenWithEmailAndPassCord
 import com.depromeet.makers.domain.usecase.GenerateTokenWithRefreshToken
+import com.depromeet.makers.domain.usecase.GetMemberByEmail
 import com.depromeet.makers.domain.usecase.UpdateDefaultMemberPassCord
 import com.depromeet.makers.presentation.restapi.dto.request.MemberDefaultPassCordRequest
 import com.depromeet.makers.presentation.restapi.dto.request.MemberLoginRequest
 import com.depromeet.makers.presentation.restapi.dto.request.MemberRefreshTokenRequest
+import com.depromeet.makers.presentation.restapi.dto.response.CheckMemberExistsByEmailResponse
 import com.depromeet.makers.presentation.restapi.dto.response.MemberLoginResponse
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @Tag(name = "인증 관련 API", description = "인증 (로그인, 패스워드 등) 관리 API")
@@ -22,6 +27,7 @@ class AuthController(
     private val generateTokenWithEmailAndPassCord: GenerateTokenWithEmailAndPassCord,
     private val generateTokenWithRefreshToken: GenerateTokenWithRefreshToken,
     private val updateDefaultMemberPassCord: UpdateDefaultMemberPassCord,
+    private val getMemberByEmail: GetMemberByEmail,
 ) {
     @Operation(summary = "이메일 & 코드로 로그인", description = "이메일과 패스키로 토큰을 발급합니다")
     @PostMapping("/login")
@@ -66,6 +72,22 @@ class AuthController(
                 email = request.email,
                 passCord = request.passCord
             )
+        )
+    }
+
+    @Operation(summary = "사용자 존재 유무 확인", description = "해당 이메일을 가진 사용자가 존재하는지 확인합니다.")
+    @GetMapping("/has-member", params = ["email"])
+    fun checkMemberExistsByEmail(
+        @RequestParam("email") email: String,
+    ): CheckMemberExistsByEmailResponse {
+        val memberExists = try {
+            getMemberByEmail.execute(GetMemberByEmail.GetMemberByEmailInput(email = email))
+            true
+        } catch(e: MemberNotFoundException) {
+            false
+        }
+        return CheckMemberExistsByEmailResponse(
+            result = memberExists
         )
     }
 }
