a739eb8468a9e20e424833c121a8403ebd3efb5b
ddingmin <ddingminyt@gmail.com>
2024-06-02 23:32:08 +0900

    refactor: 위도 경도 핸들링 변경

--- src/main/kotlin/com/depromeet/makers/presentation/restapi/dto/request/GetLocationRequest.kt
+++ src/main/kotlin/com/depromeet/makers/presentation/restapi/dto/request/GetLocationRequest.kt
@@ -6,8 +6,8 @@ import io.swagger.v3.oas.annotations.media.Schema
 data class GetLocationRequest(
 
     @Schema(description = "경도", example = "37.123456")
-    val longitude: Double?,
+    val longitude: Double,
 
     @Schema(description = "위도", example = "127.123456")
-    val latitude: Double?,
+    val latitude: Double,
 )
