9fa590312247623fd18667d32fe7cd5228147094
ddingmin <ddingminyt@gmail.com>
2024-07-15 23:47:25 +0900

    feat: 코드를 이용한 출석 처리 로직 구현
    - 기본적인 출석 체크의 검증 로직과 유사하고, 오프라인의 경우에만 작동하도록 validation

--- src/main/kotlin/com/depromeet/makers/domain/exception/AttendanceException.kt
+++ src/main/kotlin/com/depromeet/makers/domain/exception/AttendanceException.kt
@@ -11,3 +11,5 @@ class InvalidCheckInTimeException : AttendanceException(ErrorCode.INVALID_CHECKI
 class MissingPlaceParamException : AttendanceException(ErrorCode.MISSING_PLACE_PARAM)
 class InvalidCheckInDistanceException : AttendanceException(ErrorCode.INVALID_CHECKIN_DISTANCE)
 class NotFoundAttendanceException : AttendanceException(ErrorCode.NOT_EXIST_ATTENDANCE)
+class InvalidCheckInCodeException : AttendanceException(ErrorCode.INVALID_CHECKIN_CODE)
+class NotSupportedCheckInCodeException : AttendanceException(ErrorCode.NOT_SUPPORTED_CHECKIN_CODE)
--- src/main/kotlin/com/depromeet/makers/presentation/restapi/controller/CheckInController.kt
+++ src/main/kotlin/com/depromeet/makers/presentation/restapi/controller/CheckInController.kt
@@ -1,18 +1,16 @@
 package com.depromeet.makers.presentation.restapi.controller
 
 import com.depromeet.makers.domain.usecase.CheckInSession
+import com.depromeet.makers.domain.usecase.CheckInSessionWithCode
 import com.depromeet.makers.domain.usecase.GetCheckInStatus
+import com.depromeet.makers.presentation.restapi.dto.request.CheckInCodeRequest
 import com.depromeet.makers.presentation.restapi.dto.request.GetLocationRequest
 import com.depromeet.makers.presentation.restapi.dto.response.AttendanceResponse
 import com.depromeet.makers.presentation.restapi.dto.response.CheckInStatusResponse
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.security.core.Authentication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.time.LocalDateTime
 
 @Tag(name = "출석 관리 API", description = "출석 관리 API")
@@ -20,38 +18,52 @@ import java.time.LocalDateTime
 @RequestMapping("/v1/check-in")
 class CheckInController(
     private val checkInSession: CheckInSession,
+    private val checkInSessionWithCode: CheckInSessionWithCode,
     private val getCheckInStatus: GetCheckInStatus,
 ) {
-
     @Operation(summary = "세션 출석", description = "세션에 출석합니다. (서버에서 현재 시간을 기준으로 출석 처리)")
     @PostMapping
     fun checkInSession(
         authentication: Authentication,
         @RequestBody request: GetLocationRequest,
-    ): AttendanceResponse {
-        return AttendanceResponse.fromDomain(
+    ): AttendanceResponse =
+        AttendanceResponse.fromDomain(
             checkInSession.execute(
                 CheckInSession.CheckInSessionInput(
                     now = LocalDateTime.now(),
                     memberId = authentication.name,
                     latitude = request.latitude,
                     longitude = request.longitude,
-                )
-            )
+                ),
+            ),
         )
-    }
+
+    @Operation(summary = "코드 기반 세션 출석", description = "세션에 출석합니다. (출석 코드를 기반으로 출석 처리)")
+    @PostMapping("/code")
+    fun checkInSessionWithCode(
+        authentication: Authentication,
+        @RequestBody request: CheckInCodeRequest,
+    ): AttendanceResponse =
+        AttendanceResponse.fromDomain(
+            checkInSessionWithCode.execute(
+                CheckInSessionWithCode.CheckInSessionWithCodeInput(
+                    memberId = authentication.name,
+                    now = LocalDateTime.now(),
+                    code = request.code,
+                ),
+            ),
+        )
 
     @Operation(summary = "세션 출석 상태", description = "현재 세션 출석 상태 확인합니다. (CheckInStatusResponse 스키마에 자세한 설명있습니다.)")
     @GetMapping
-    fun checkInStatus(
-        authentication: Authentication,
-    ): CheckInStatusResponse {
-        val checkInStatus = getCheckInStatus.execute(
-            GetCheckInStatus.GetCheckInStatusInput(
-                now = LocalDateTime.now(),
-                memberId = authentication.name,
+    fun checkInStatus(authentication: Authentication): CheckInStatusResponse {
+        val checkInStatus =
+            getCheckInStatus.execute(
+                GetCheckInStatus.GetCheckInStatusInput(
+                    now = LocalDateTime.now(),
+                    memberId = authentication.name,
+                ),
             )
-        )
         return CheckInStatusResponse(
             generation = checkInStatus.generation,
             week = checkInStatus.week,
