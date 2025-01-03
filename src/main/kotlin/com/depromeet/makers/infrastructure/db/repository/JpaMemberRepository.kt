3aae0f07e457295e47369ba7118ac45e199a588f
ddingmin <ddingminyt@gmail.com>
2025-01-03 22:03:31 +0900

    bug: 이전 기수로 멤버가 찾아지는 현상 수정 (#132)

--- src/main/kotlin/com/depromeet/makers/infrastructure/db/repository/JpaMemberRepository.kt
+++ src/main/kotlin/com/depromeet/makers/infrastructure/db/repository/JpaMemberRepository.kt
@@ -4,5 +4,5 @@ import com.depromeet.makers.infrastructure.db.entity.MemberEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface JpaMemberRepository : JpaRepository<MemberEntity, String> {
-    fun findByEmail(email: String): MemberEntity?
+    fun findTopByEmailOrderByGenerationsDesc(email: String): MemberEntity?
 }
