eeeb2ceb95ab7a88aaa0e6316bc249fa2fc9ff73
ddingmin <ddingmin.dev@gmail.com>
2025-03-16 22:31:08 +0900

    feat: app 정보에 디프만 기수 정보 추가

--- src/main/kotlin/com/depromeet/makers/config/properties/AppProperties.kt
+++ src/main/kotlin/com/depromeet/makers/config/properties/AppProperties.kt
@@ -4,7 +4,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "app")
 data class AppProperties(
-    val token: TokenProperties
+    val token: TokenProperties,
+    val depromeet: DepromeetProperties,
 ) {
     data class TokenProperties(
         val secretKey: String,
@@ -15,5 +16,9 @@ data class AppProperties(
             val refresh: Long,
         )
     }
+
+    data class DepromeetProperties(
+        val generation: Int,
+    )
 }
 
