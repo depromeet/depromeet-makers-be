f65d22f6bf708e5aec3dc08df18464c8235e722f
ddingmin <ddingmin.dev@gmail.com>
2025-03-21 17:40:21 +0900

    feat: id 값을 String -> ObjectId 로 변경

--- src/main/kotlin/com/depromeet/makers/domain/Member.kt
+++ src/main/kotlin/com/depromeet/makers/domain/Member.kt
@@ -2,13 +2,14 @@ package com.depromeet.makers.domain
 
 import com.depromeet.makers.domain.enums.MemberRole
 import com.depromeet.makers.domain.vo.Age
+import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document("member")
 class Member(
     @Id
-    val id: String?,
+    val id: ObjectId,
     val name: String,
     val age: Age,
     val role: MemberRole,
